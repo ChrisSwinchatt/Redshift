68dbb08711335b0298fad78256a162df6655f9ea
Chris Swinchatt <c.swinchatt@sussex.ac.uk>
2017-12-18 17:20:49 +0000

    Add hashing function

---
+++ src/util/hash.c
@@ -0,0 +1,32 @@
+/**
+ * \file util/hash.c
+ * \brief String hashing functions.
+ * \author Chris Swinchatt <c.swinchatt@sussex.ac.uk>
+ * \copyright Copyright (c) 2012-2018 Chris Swinchatt.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+ * documentation files (the "Software"), to deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the
+ * Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+ * WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+ * OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+#include <string.h>
+#include <util/hash.h>
+
+uint32_t hash32_asciz(const char* asciz, size_t size)
+{
+    uint32_t a = 33;
+    uint32_t h = 5381;
+    size_t   i = 0;
+    for (; i < size && asciz[i] != 0; ++i) {
+        h = asciz[i] + h*a;
+    }
+    return h;
+}
