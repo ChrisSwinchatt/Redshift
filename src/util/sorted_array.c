837c1246ad50bb20ddf3f6d79daa2d15ef97fbf0
Chris Swinchatt <c.swinchatt@sussex.ac.uk>
2018-01-21 20:51:19 +0000

    Fix bugs in sorted_array
    Bugs fixed:
     * Dereferencing void pointer (void** elements => uintptr_t* elements in struct sorted_array) (at several places)
     * Allocating sizeof(struct sorted_array*) instead of sizeof(struct sorted_array) (in create_sorted_array)
     * Copying error when inserting at the head or in the middle of the array (in sorted_array_add)

--- src/util/sorted_array.c
+++ src/util/sorted_array.c
@@ -24,11 +24,11 @@
 #include <string.h>
 
 struct sorted_array {
-    void**          elements;
-    uint8_t         freeable;
-    size_t          count;
-    size_t          capacity;
-    order_predicate predicate;
+    uintptr_t*      elements;   /* Element addresses.                                                 */
+    uint8_t         freeable;   /* Whether the array was allocated statically (0) or dynamically (1). */
+    size_t          count;      /* How many element addresses are currently stored.                   */
+    size_t          capacity;   /* Maximum number of element addresses that can be stored.            */
+    order_predicate predicate;  /* Function which determines where an element should be inserted.     */
 };
 
 bool uint32_t_ascending_order_predicate(void* pa, void* pb)
@@ -37,7 +37,7 @@ bool uint32_t_ascending_order_predicate(void* pa, void* pb)
     DEBUG_ASSERT(pb != NULL);
     uint32_t a = *(uint32_t*)pa;
     uint32_t b = *(uint32_t*)pb;
-    return (a < b);
+    return a < b;
 }
 
 bool uint32_t_descending_order_predicate(void* pa, void* pb)
@@ -46,20 +46,22 @@ bool uint32_t_descending_order_predicate(void* pa, void* pb)
     DEBUG_ASSERT(pb != NULL);
     uint32_t a = *(uint32_t*)pa;
     uint32_t b = *(uint32_t*)pb;
-    return (a > b);
+    return a > b;
 }
 
 struct sorted_array* create_sorted_array(size_t capacity, bool static_, order_predicate predicate)
 {
-    const size_t size = sizeof(struct sorted_array*);
-    void* address;
+    struct sorted_array* array;
+    const size_t size = sizeof(*array);
+    void* address = NULL;
     if (static_) {
         address = static_alloc(size);
     } else {
         address = kmalloc(size);
     }
     DEBUG_ASSERT(address != NULL);
-    return place_sorted_array(address, capacity, static_, predicate);
+    array = place_sorted_array(address, capacity, static_, predicate);
+    return array;
 }
 
 struct sorted_array* place_sorted_array(void* address, size_t capacity, bool static_, order_predicate predicate)
@@ -68,16 +70,17 @@ struct sorted_array* place_sorted_array(void* address, size_t capacity, bool sta
     DEBUG_ASSERT(capacity > 0);
     DEBUG_ASSERT(predicate != NULL);
     struct sorted_array* list = address;
-    size_t count = sizeof(*(list->elements))*capacity;
+    const size_t size = sizeof(*(list->elements))*capacity;
+    memset(list, 0, sizeof(*list));
     if (static_) {
-        list->elements = static_alloc(count);
+        list->elements = static_alloc(size);
         list->freeable = 0;
     } else {
-        list->elements = kmalloc(count);
+        list->elements = kmalloc(size);
         list->freeable = 1;
     }
     DEBUG_ASSERT(list->elements != NULL);
-    memset(list->elements, 0, sizeof(*(list->elements))*capacity);
+    memset(list->elements, 0, size);
     list->count     = 0;
     list->capacity  = capacity;
     list->predicate = predicate;
@@ -86,10 +89,11 @@ struct sorted_array* place_sorted_array(void* address, size_t capacity, bool sta
 
 void delete_sorted_array(struct sorted_array* list)
 {
-    if (list && list->freeable) {
-        kfree(list->elements);
-        kfree(list);
-    }
+    DEBUG_ASSERT(list != NULL);
+    DEBUG_ASSERT(list->elements != NULL);
+    DEBUG_ASSERT(list->freeable == 1);
+    kfree(list->elements);
+    kfree(list);
 }
 
 size_t sorted_array_add(struct sorted_array* list, void* element)
@@ -98,25 +102,33 @@ size_t sorted_array_add(struct sorted_array* list, void* element)
     DEBUG_ASSERT(list->elements != NULL);
     DEBUG_ASSERT(list->predicate != NULL);
     DEBUG_ASSERT(element != NULL);
+    const uintptr_t element_address = (uintptr_t)element;
+    void* predicate = list->predicate;
     if (list->count == 0) {
-        list->elements[0] = element;
+        list->elements[0] = element_address;
         list->count++;
+        DEBUG_ASSERT(list->predicate == predicate);
         return list->count;
     }
     size_t i = 0;
-    for (; i < list->count && list->predicate(list->elements[i], element); ++i) {
-        DO_NOTHING;
+    for (i = 0; i < list->count; ++i) {
+        DEBUG_ASSERT(list->elements[i] != 0);
+        if (!(list->predicate((void*)list->elements[i], element))) {
+            break;
+        }
     }
     if (i >= list->count) {
         /* Append.
          */
-        list->elements[list->count++] = element;
+        list->elements[list->count++] = element_address;
     } else {
-        /* Move each pointer from the ith element to the end of the list one space to the "right". Then insert the new
+        /* Move each address from the ith element to the end of the list one space to the "right". Then insert the new
          * element at position i.
          */
-        memmove(list->elements + i + 1, list->elements + i, list->count - i - 1);
-        list->elements[i] = element;
+        for (size_t j = list->count; j > i; --j) {
+            list->elements[j] = list->elements[j - 1];
+        }
+        list->elements[i] = element_address;
         list->count++;
     }
     /* Ensure we haven't run past the end of the array. TODO return failure when this happens instead of just crashing.
@@ -128,7 +140,7 @@ size_t sorted_array_add(struct sorted_array* list, void* element)
 void* sorted_array_get(const struct sorted_array* list, size_t index)
 {
     DEBUG_ASSERT(index < list->count);
-    return list->elements[index];
+    return (void*)(list->elements[index]);
 }
 
 void sorted_array_remove(struct sorted_array* list, size_t index)
